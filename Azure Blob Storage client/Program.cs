b7d14322b3ebb28a54f1d450dedf6a45bceabf9c
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-07-05 12:26:01 +0200

    Azure Blob storage client

---
+++ Azure Blob Storage client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Identity;
+
+var storageaccountName = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_NAME");
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(
+    new Uri($"https://{storageaccountName}.blob.core.windows.net"),
+    new DefaultAzureCredential());
+//Console.WriteLine(blobServiceClient.Uri);
---
+++ Azure Blob Storage client/README.md
@@ -0,0 +1,10 @@
+# Azure Blob storage Demo - Client library for .NET
+
+* ``dotnet new console``
+* ``dotnet add package Azure.Storage.Blobs``
+* ``dotnet add package Azure.Identity``
+
+Set env. var ``STORAGE_ACCOUNT_NAME``
+
+Authenticate to Azure
+__Heads up!__ Must have "Storage Blob Data Contributor" role.
