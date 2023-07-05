18b866d3dbeb0f5391d4468097964dbc52acbdb0
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-07-05 13:28:18 +0200

    Azure Blob storage client

--- Azure Blob Storage client/Program.cs
+++ Azure Blob Storage client/Program.cs
@@ -6,7 +6,8 @@ using Azure.Storage.Blobs.Models;
 using Azure.Identity;
 
 string storageaccountName = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_NAME");
-if (String.IsNullOrEmpty(storageaccountName)) {
+if (String.IsNullOrEmpty(storageaccountName))
+{
     throw new ArgumentNullException("storageaccountName", "Must provide a storage account name");
 }
 
@@ -14,14 +15,36 @@ if (String.IsNullOrEmpty(storageaccountName)) {
 BlobServiceClient blobServiceClient = new BlobServiceClient(
     new Uri($"https://{storageaccountName}.blob.core.windows.net"),
     new DefaultAzureCredential());
-//Console.WriteLine(blobServiceClient.Uri);
+Console.WriteLine($"\nConnected to storage account {blobServiceClient.Uri}");
+
 
 // #### Create a container in storage account and return a container client object
+Console.Write("#### Press any key to add container to storage account");
+Console.ReadLine();
 string containerName = $"demo-container-{Guid.NewGuid().ToString()}";
 BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
 
+
 // #### Upload a blob to the container
+Console.Write("\n#### Press any key to add blob to container");
+Console.ReadLine();
 string blobFileName = $"dummy-{Guid.NewGuid().ToString()}.txt";
 BlobClient blobClient = containerClient.GetBlobClient(blobFileName);
 string localFilePath = "./dummy.txt";
 await blobClient.UploadAsync(localFilePath, true);
+
+
+// #### List blobs in the container
+Console.Write("\n#### Press any key to list blobs in container");
+Console.ReadLine();
+await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+{
+    Console.WriteLine($"\t{blobItem.Name}");
+}
+
+
+// #### Clean up (delete it all)
+Console.Write("\n#### Press any key to delete everything");
+Console.ReadLine();
+Console.WriteLine("Deleting blob container...");
+await containerClient.DeleteAsync();
